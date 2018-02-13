befd1b0650cfd4f5615b78df79f6aefb54e4b886
Eric Christensen <eric@aehe.us>
2018-02-13 14:32:31 -0500

    Fixed indentions

--- get_energy_data.py
+++ get_energy_data.py
@@ -24,11 +24,17 @@ connection = pymysql.connect(host='localhost',user='sol',password='TheSun',db='E
 try:
     with connection.cursor() as cursor:
 # Create new record
-    sql = "INSERT INTO Gateway_Data(battery_instant_power, battery_energy_imported, battery_energy_exported, load_instant_power, load_energy_imported, load_energy_exported, solar_instant_power, solar_energy_imported, solar_energy_exported, grid_instant_power, grid_energy_imported, grid_energy_exported) \
-   VALUES ('data['battery']['instant_power'], data['battery']['energy_imported'], 'data['battery']['energy_exported']', 'data['load']['instant_power']', 'data['load']['energy_imported']', 'data['load']['energy_exported']','data['solar']['instant_power']', 'data['solar']['energy_imported']', 'data['solar']['energy_exported']','data['site']['instant_power']', 'data['site']['energy_imported']', 'data['site']['energy_exported']')"
+        sql = "INSERT INTO Gateway_Data(battery_instant_power, battery_energy_imported, battery_energy_exported, \
+        load_instant_power, load_energy_imported, load_energy_exported, \
+        solar_instant_power, solar_energy_imported, solar_energy_exported, \
+        grid_instant_power, grid_energy_imported, grid_energy_exported) \
+        VALUES ('data['battery']['instant_power'], data['battery']['energy_imported'], 'data['battery']['energy_exported']',\
+        'data['load']['instant_power']', 'data['load']['energy_imported']', 'data['load']['energy_exported']', \
+        'data['solar']['instant_power']', 'data['solar']['energy_imported']', 'data['solar']['energy_exported']', \
+        'data['site']['instant_power']', 'data['site']['energy_imported']', 'data['site']['energy_exported']')"
 
    # Execute the SQL command
-   connection.commit()
+        connection.commit()
 
 finally:
 # disconnect from server
