1c756d5c40ce0b013ce3a974fc72170394e4f2a0
Eric Christensen <eric@aehe.us>
2018-02-13 12:38:26 -0500

    Comment out the massive amount of data being printed

--- get_energy_data.py
+++ get_energy_data.py
@@ -7,10 +7,11 @@ api_url_base = 'http://192.168.1.214/api/meters/aggregates'
 
 r = requests.get(url=api_url_base)
 data = r.json()
-pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(data)
+#pp = pprint.PrettyPrinter(indent=2)
+#pp.pprint(data)
 # An example of pulling out individual values
 print("Solar power: ",data['solar']['instant_power'])
 print("House load: ",data['load']['instant_power'])
 print("Powerwall: ",data['battery']['instant_power'])
 print("BGE: ",data['site']['instant_power'])
+
