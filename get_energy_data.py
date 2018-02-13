780c4f8799a71283624bf58124e2c31c7188c225
Eric Christensen <eric@aehe.us>
2018-02-13 14:26:50 -0500

    Changed pymysql instructions to align with version 0.8.z docs

--- get_energy_data.py
+++ get_energy_data.py
@@ -2,7 +2,7 @@
 import json
 import requests
 import pprint
-import pymysql
+import pymysql.cursors
 
 api_url_base = 'http://192.168.1.214/api/meters/aggregates'
 
@@ -19,23 +19,18 @@ data = r.json()
 # Insert this data into a db.
 
 # Open database connection
-db = pymysql.connect("localhost","sol","TheSun","Energy_Usage")
-
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
-
-# Prepare SQL query to INSERT energy data into db
-sql = "INSERT INTO Gateway_Data(battery_instant_power, battery_energy_imported, battery_energy_exported, load_instant_power, load_energy_imported, load_energy_exported, solar_instant_power, solar_energy_imported, solar_energy_exported, grid_instant_power, grid_energy_imported, grid_energy_exported) \
-   VALUES ('data['battery']['instant_power'], data['battery']['energy_imported'], 'data['battery']['energy_exported']', 'data['load']['instant_power']', 'data['load']['energy_imported']', 'data['load']['energy_exported']','data['solar']['instant_power']', 'data['solar']['energy_imported']', 'data['solar']['energy_exported']','data['site']['instant_power']', 'data['site']['energy_imported']', 'data['site']['energy_exported']')"
+connection = pymysql.connect(host='localhost',user='sol',password='TheSun',db='Energy_Usage',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 
 try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Commit your changes in the database
-   db.commit()
-except:
-   # Rollback in case there is any error
-   db.rollback()
+    with connection.cursor() as cursor:
+# Create new record
+    sql = "INSERT INTO Gateway_Data(battery_instant_power, battery_energy_imported, battery_energy_exported, load_instant_power, load_energy_imported, load_energy_exported, solar_instant_power, solar_energy_imported, solar_energy_exported, grid_instant_power, grid_energy_imported, grid_energy_exported) \
+   VALUES ('data['battery']['instant_power'], data['battery']['energy_imported'], 'data['battery']['energy_exported']', 'data['load']['instant_power']', 'data['load']['energy_imported']', 'data['load']['energy_exported']','data['solar']['instant_power']', 'data['solar']['energy_imported']', 'data['solar']['energy_exported']','data['site']['instant_power']', 'data['site']['energy_imported']', 'data['site']['energy_exported']')"
 
+   # Execute the SQL command
+   connection.commit()
+
+finally:
 # disconnect from server
-db.close()
+    connection.close()
+
