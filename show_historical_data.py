be5bd6c9d1a8a92797de0ab164506ed4705f71d1
Eric Christensen <eric@aehe.us>
2018-02-14 21:36:26 -0500

    Added queries for grid, Powerwall, and house

--- show_historical_data.py
+++ show_historical_data.py
@@ -9,8 +9,7 @@ connection = pymysql.connect(host='localhost',user='sol',password='TheSun',db='E
 
 try:
     with connection.cursor() as cursor:
-     # Read starting solar number for the day.
-
+     # Compute amount of energy captured from solar panels.
      sql = "SELECT min(solar_energy_exported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
      cursor.execute(sql)
      start_solar_odometer = cursor.fetchone()
@@ -18,7 +17,63 @@ try:
      sql = "SELECT max(solar_energy_exported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
      cursor.execute(sql)
      current_solar_odometer = cursor.fetchone()
+     print("Energy captured by the solar panels: ")
      print((current_solar_odometer['max(solar_energy_exported)']) - (start_solar_odometer['min(solar_energy_exported)']))
+
+     # Compute amount of energy used from the grid.
+     sql = "SELECT min(grid_energy_imported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     start_solar_odometer = cursor.fetchone()
+
+     sql = "SELECT max(grid_energy_imported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     current_solar_odometer = cursor.fetchone()
+     print("Energy used from the grid: ")
+     print((current_solar_odometer['max(grid_energy_imported)']) - (start_solar_odometer['min(grid_energy_imported)']))
+
+     # Compute amount of energy sent to the grid.
+     sql = "SELECT min(grid_energy_exported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     start_solar_odometer = cursor.fetchone()
+
+     sql = "SELECT max(grid_energy_exported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     current_solar_odometer = cursor.fetchone()
+     print("Energy sent to the grid: ")
+     print((current_solar_odometer['max(grid_energy_exported)']) - (start_solar_odometer['min(grid_energy_exported)']))
+
+     # Compute amount of energy sent to Powerwall
+     sql = "SELECT min(battery_energy_imported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     start_solar_odometer = cursor.fetchone()
+
+     sql = "SELECT max(battery_energy_imported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     current_solar_odometer = cursor.fetchone()
+     print("Energy sent to Powerwall: ")
+     print((current_solar_odometer['max(battery_energy_imported)']) - (start_solar_odometer['min(battery_energy_imported)']))
+
+     # Compute amount of energy retrieved from Powerwall
+     sql = "SELECT min(battery_energy_exported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     start_solar_odometer = cursor.fetchone()
+
+     sql = "SELECT max(battery_energy_exported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     current_solar_odometer = cursor.fetchone()
+     print("Energy retrieved from Powerwall: ")
+     print((current_solar_odometer['max(battery_energy_exported)']) - (start_solar_odometer['min(battery_energy_exported)']))
+
+     # Compute amount of energy used by the house
+     sql = "SELECT min(load_energy_imported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     start_solar_odometer = cursor.fetchone()
+
+     sql = "SELECT max(load_energy_imported) FROM `Gateway_Data` WHERE DATE(`date_time`) = CURDATE()"
+     cursor.execute(sql)
+     current_solar_odometer = cursor.fetchone()
+     print("Energy used by the house: ")
+     print((current_solar_odometer['max(load_energy_imported)']) - (start_solar_odometer['min(load_energy_imported)']))
 
 finally:
      connection.close()
