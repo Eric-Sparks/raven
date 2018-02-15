35893e59fc75435316fb39afc43f1f0093947dab
Eric Christensen <eric@aehe.us>
2018-02-14 19:59:49 -0500

    Removed 'load energy export' field as it will always be zero

--- get_energy_data.py
+++ get_energy_data.py
@@ -28,14 +28,14 @@ while True:
         with connection.cursor() as cursor:
         # Create new record
             sql = "INSERT INTO Gateway_Data(battery_instant_power, battery_energy_imported, battery_energy_exported, \
-            load_instant_power, load_energy_imported, load_energy_exported, \
+            load_instant_power, load_energy_imported, \
             solar_instant_power, solar_energy_imported, solar_energy_exported, \
             grid_instant_power, grid_energy_imported, grid_energy_exported) \
-            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
        # Execute the SQL command
             cursor.execute(sql,(data['battery']['instant_power'], data['battery']['energy_imported'], data['battery']['energy_exported'],\
-            data['load']['instant_power'], data['load']['energy_imported'], data['load']['energy_exported'], \
+            data['load']['instant_power'], data['load']['energy_imported'], \
             data['solar']['instant_power'], data['solar']['energy_imported'], data['solar']['energy_exported'], \
             data['site']['instant_power'], data['site']['energy_imported'], data['site']['energy_exported']))
 
