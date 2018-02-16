cb688b405603fe100eceefc673888c93a8e7a30c
Eric Christensen <eric@aehe.us>
2018-02-16 14:22:25 -0500

    Add license information

--- get_energy_data.py
+++ get_energy_data.py
@@ -1,3 +1,21 @@
+#    Raven imports energy usage, storage, and export data from Tesla Powerwall 2,
+#    stores it, and offers to display it in a variety of formats.
+#
+#    Copyright (C) 2018 Eric H. Christensen, Jared Smith
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 # This script connects to the Telsa Powerwall 2 gateway API and records
 # the real-time electron between the solar panels, Powerwall, grid,
 # and the house (load).
